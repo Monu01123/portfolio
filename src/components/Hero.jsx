1df16f5848c0edcb24bf47502b604e9841ee4c4d
Monu01123 <monumeena0112@gmail.com>
2026-02-09 21:40:08 +0530

    Fix build error: Remove unused ArrowForwardIcon import

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+
 import HeroImage from "./gh.png";
 
 const Hero = () => {
